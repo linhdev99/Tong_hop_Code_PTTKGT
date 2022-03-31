ae9d823ec089967755948fc0583c5864331ca762
DESKTOP-CKNLIRI\PeterLinh <linh.huynh.dev.99@gmail.com>
2022-03-31 22:27:34 +0700

    add perm

---
+++ perm.py
@@ -0,0 +1,19 @@
+from hashlib import new
+from operator import le
+from re import S
+
+
+def perm(a):
+    Ps = [str(a[0])]
+    for i in a[1:]:
+        newPs = []
+        for p in Ps:
+            for x in range(len(p),-1,-1):
+                temp = p
+                temp = temp[:x] + str(i) + temp[x:]
+                newPs += [temp]
+        Ps = newPs
+    print("So hoan vi: ", len(Ps))
+    print("Cac hoan vi")
+    print(Ps)
+perm([1,2,3,4])
