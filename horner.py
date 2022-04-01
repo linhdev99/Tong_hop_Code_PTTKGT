9fdb7f1ab6d36d01c0f6d0cd45696c88e204aa3c
DESKTOP-CKNLIRI\PeterLinh <linh.huynh.dev.99@gmail.com>
2022-04-01 23:16:01 +0700

    horner

---
+++ horner.py
@@ -0,0 +1,22 @@
+def horner1(P, x):
+    res = P[0]
+    for i in range(1, len(P)):
+        res = x*res+P[i]
+    print(res)
+
+
+def horner2(P, x):
+    res = P[-1]
+    for i in range(len(P)-2, -1, -1):
+        res = x*res+P[i]
+    print(res)
+
+
+arr1 = [2, -1, 3, 1, -5]
+arr2 = [3, -1, 0, 2, 5]
+
+horner1(arr1, 3)
+horner1(arr2, -2)
+
+horner2(arr1[::-1], 3)
+horner2(arr2[::-1], -2)
